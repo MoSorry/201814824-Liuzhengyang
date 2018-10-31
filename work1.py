ce7afb86b11a0936acc354d3584f8ab44c425689
MoSorry <43344485+MoSorry@users.noreply.github.com>
2018-10-31 19:05:15 +0800

    Update work1.py

--- work1.py
+++ work1.py
@@ -13,26 +13,25 @@ from numpy import *
 from os import listdir,mkdir,path
 import re
 import operator
-originSample = 'D:/data mining/20news-18828' #确定数据存储的目录
-processedSample = 'D:/data mining/20news'     #新的存放数据的文件夹
+origin = 'D:/data mining/20news-18828' #确定数据存储的目录
+sample = 'D:/data mining/20news'     #新的存放数据的文件夹
 
 def createFiles():                                #将数据复制到新文件夹，避免原始数据破坏
-    srcFilesList = listdir('originSample')      #将目录下每个文件夹名称以组的形式保存
+    srcFilesList = listdir('origin')      #将目录下每个文件夹名称以组的形式保存
     for i in range(len(srcFilesList)):
         if i==0: continue
-        dataFilesDir = 'originSample/' + srcFilesList[i]  # 确定每个文件夹的路径
+        dataFilesDir = 'origin/' + srcFilesList[i]  # 确定每个文件夹的路径
         dataFilesList = listdir(dataFilesDir)               #确定每个新闻类别路径下的具体文件名称
-        targetDir = 'processedSample/' + srcFilesList[i] # 新文件夹的每个的路径
+        targetDir = 'sample/' + srcFilesList[i] # 新文件夹的每个的路径
         if path.exists(targetDir)==False:
             mkdir(targetDir)                                #创建相应的文件夹
         for j in range(len(dataFilesList)):
             createProcessFile(srcFilesList[i],dataFilesList[j])#调用createProcessFile()在新文档中处理文本
 
-#在前期处理中因为anaconda中并没有textblob的库，所以借鉴网上的方法，将数据重新复制排版代替分词操作。
 
 def createProcessFile(srcFilesName,dataFilesName):
-    srcFile = 'originSample/' + srcFilesName + '/' + dataFilesName
-    targetFile= 'processedSample/' + srcFilesName\
+    srcFile = 'origin/' + srcFilesName + '/' + dataFilesName
+    targetFile= 'sample/' + srcFilesName\
                 + '/' + dataFilesName
     fw = open(targetFile,'w')
     dataList = open(srcFile).readlines()
@@ -54,7 +53,7 @@ def lineProcess(line):
 
 ##################################################### IDF###############################################################
 def computeIDF():
-    fileDir = 'processedSample'
+    fileDir = 'sample'
     wordDocMap = {}  # <word, (docM,...,docN)>
     IDFPerWord = {}  # <word, IDF>
     countDoc = 0.0
@@ -74,7 +73,7 @@ def computeIDF():
 
     for word in wordDocMap.keys():
         countDoc = len(wordDocMap[word])  # 统计文档个数
-        IDF = log(20000 / countDoc)
+        IDF = log(20000 / countDoc)/log(10)
         IDFPerWord[word] = IDF
     return IDFPerWord
 
@@ -83,7 +82,55 @@ def main():
     IDFPerWord = computeIDF()
     end=time.clock()
     print ('runtime: ' + str(end-start))
-    fw = open('IDFPerWord','w')
+    voc='sample/' + vocabulary
+    mkdir(voc)
+    fw = open('voc','w')
     for word, IDF in IDFPerWord.items():
         fw.write('%s %.6f\n' % (word,IDF))
     fw.close()
+
+def computeTFMultiIDF(indexOfSample, trainSamplePercent):
+        IDFPerWord = {}  # <word, IDF值> 从文件中读入后的数据保存在此字典结构中
+        for line in open('IDFPerWord').readlines():
+            (word, IDF) = line.strip('\n').split(' ')
+            IDFPerWord[word] = IDF
+
+        fileDir = 'processedSampleOnlySpecial_2'
+        trainFileDir = "docVector/" + 'wordTFIDFMapTrainSample' + str(indexOfSample)
+        testFileDir = "docVector/" + 'wordTFIDFMapTestSample' + str(indexOfSample)
+
+        tsTrainWriter = open(trainFileDir, 'w')
+        tsTestWriter = open(testFileDir, 'w')
+
+        cateList = listdir(fileDir)
+        for i in range(len(cateList)):
+            sampleDir = fileDir + '/' + cateList[i]
+            sampleList = listdir(sampleDir)
+
+            testBeginIndex = indexOfSample * (len(sampleList) * (1 - trainSamplePercent))
+            testEndIndex = (indexOfSample + 1) * (len(sampleList) * (1 - trainSamplePercent))
+
+            for j in range(len(sampleList)):
+                TFPerDocMap = {}  # <word, 文档doc下该word的出现次数>
+                sumPerDoc = 0  # 记录文档doc下的单词总数
+                sample = sampleDir + '/' + sampleList[j]
+                for line in open(sample).readlines():
+                    sumPerDoc += 1
+                    word = line.strip('\n')
+                    TFPerDocMap[word] = TFPerDocMap.get(word, 0) + 1
+
+                if (j >= testBeginIndex) and (j <= testEndIndex):
+                    tsWriter = tsTestWriter
+                else:
+                    tsWriter = tsTrainWriter
+
+                tsWriter.write('%s %s ' % (cateList[i], sampleList[j]))  # 写入类别cate，文档doc
+
+                for word, count in TFPerDocMap.items():
+                    TF = float(count) / float(sumPerDoc)
+                    tsWriter.write('%s %f ' % (word, TF * float(IDFPerWord[word])))  # 继续写入类别cate下文档doc下的所有单词及它的TF-IDF值
+
+                tsWriter.write('\n')
+        tsTrainWriter.close()
+        tsTestWriter.close()
+        tsWriter.close()
